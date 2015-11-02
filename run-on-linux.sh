3892f5d6873211e57b42e326ba35dc61d7fb24c1
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2015-11-01 20:24:03 -0800

    Add benchmark-thrash2 to bash scripts for running benchmarks

--- run-on-linux.sh
+++ run-on-linux.sh
@@ -11,6 +11,7 @@ cp /dev/null ${basename}-core-5runs.txt
 for stride in 1 16 64
 do
     cp /dev/null ${basename}-thrash-${stride}-5runs.txt
+    cp /dev/null ${basename}-thrash2-${stride}-5runs.txt
 done
 
 for run in 1 2 3 4 5
@@ -19,5 +20,6 @@ do
     for stride in 1 16 64
     do
         ${DIR}/benchmark-thrash ${stride} >> ${basename}-thrash-${stride}-5runs.txt
+        ${DIR}/benchmark-thrash2 ${stride} >> ${basename}-thrash2-${stride}-5runs.txt
     done
 done
